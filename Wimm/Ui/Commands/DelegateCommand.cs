14a4c0c084dc04549c0b375a74f92ace98bc9570
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-01-17 20:51:20 +0900

    add DelegateCommands

---
+++ Wimm/Ui/Commands/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wimm.Ui.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action handler;
+        private Func<bool>? canExecute;
+        public DelegateCommand(Action handler,Func<bool>? canExecuteHandler=null)
+        {
+            this.handler = handler;
+            this.canExecute = canExecuteHandler;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            handler();
+        }
+    }
+}
---
+++ Wimm/Ui/Commands/ParamsDelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wimm.Ui.Commands
+{
+    public class ParamsDelegateCommand :ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action<object?> handler;
+        private Func<object?,bool>? canExecute;
+        public ParamsDelegateCommand(Action<object?> handler, Func<object?,bool>? canExecuteHandler = null)
+        {
+            this.handler = handler;
+            this.canExecute = canExecuteHandler;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            handler(parameter);
+        }
+    }
+}
