4d64e6217aaca16c7460f6008fa203aa811e1d53
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-04-30 10:32:19 +0900

    implement Tpip4RP

---
+++ Wimm.Machines.TpipForRasberryPi/TPIP4Camera.cs
@@ -0,0 +1,60 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Wimm.Machines.TpipForRasberryPi.Import;
+using Wimm.Machines.Video;
+
+namespace Wimm.Machines.TpipForRasberryPi
+{
+    public class Tpip4Camera : Camera
+    {
+        public Tpip4Camera(params string[] names)
+        {
+            if (names.Length > 4) { throw new ArgumentException("カメラは4つまでしか設定できません"); }
+            Channels = names.Select(it => (Channel)new Tpip4CameraChannel(it)).ToImmutableArray();
+        }
+        public override bool SupportingMultiObservation => false;
+        private const int WM_PAINT = 0x000F;
+        private ulong lastTimeStamp = 0;
+        private int activeChannelIndex = 0;
+        public IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_PAINT)
+            {
+                if (!CanDataSend) { return IntPtr.Zero; }
+                ulong timeStamp = TPJT4.NativeMethods.JF_get_stamp();
+                if (lastTimeStamp < timeStamp)
+                {
+                    lastTimeStamp = timeStamp;
+                    int byteSize = 0;
+                    var arrayAddress = TPJT4.NativeMethods.get_jpeg_file(0, 0, ref byteSize);
+                    if (arrayAddress != IntPtr.Zero && byteSize > 0)
+                    {
+                        byte[] data = new byte[byteSize];
+                        Marshal.Copy(arrayAddress, data, 0, byteSize);
+                        var image = Cv2.ImDecode(data, ImreadModes.Unchanged);
+                        CallUpdateHandler(new[] { new FrameData(Channels[activeChannelIndex], image) });
+                        handled = true;
+                    }
+                    TPJT4.NativeMethods.free_jpeg_file();
+                }
+            }
+            return IntPtr.Zero;
+        }
+        public override void Activate(Channel channel, bool activation)
+        {
+            base.Activate(channel, activation);
+            activeChannelIndex = Channels.IndexOf(channel);
+            if (activation) TPJT4.NativeMethods.chg_camera(activeChannelIndex);
+        }
+    }
+    public class Tpip4CameraChannel : Channel
+    {
+        public Tpip4CameraChannel(string name) : base(name) { }
+    }
+}
--- Wimm.Machines.TpipForRasberryPi/TpipForRasberryPiMachine.cs
+++ Wimm.Machines.TpipForRasberryPi/TpipForRasberryPiMachine.cs
@@ -1,7 +1,44 @@
-﻿namespace Wimm.Machines.TpipForRasberryPi
-{
-    public class TpipForRasberryPiMachine : Machine
-    {
+﻿using System.Windows.Interop;
+using Wimm.Machines.TpipForRasberryPi.Import;
 
+namespace Wimm.Machines.TpipForRasberryPi
+{
+    /// <summary>
+    /// Tpip4ボードで制御するロボットを表現するクラスです。
+    /// </summary>
+    /// <remarks>派生クラスはpublic (string,HwndSource)コンストラクタを提供する必要があります</remarks>
+    public abstract class TpipForRasberryPiMachine : Machine
+    {
+        /// <summary>
+        /// Tpipの初期化が行われたか、falseの場合実際の制御処理を行う必要はありません。
+        /// </summary>
+        protected bool TpipInitialized { get; private init; }
+        public const string ControlBoardName = "Tpip4RP";
+        public override string ControlBoard => ControlBoardName;
+        public override ConnectionState ConnectionStatus
+        {
+            get
+            {
+                return TPJT4.NativeMethods.get_com_mode() switch
+                {
+                    0 => ConnectionState.Offline,
+                    1 => ConnectionState.ConnectionReady,
+                    2 or 3 => ConnectionState.Connecting,
+                    4 => ConnectionState.Online,
+                    _ => ConnectionState.Unknown
+                };
+            }
+        }
+        protected HwndSource? Hwnd { get; init; }
+        protected TpipForRasberryPiMachine(string machineIpAddress, HwndSource hwnd)
+        {
+            Hwnd = hwnd;
+            TpipInitialized = true;
+            TPJT4.NativeMethods.init(machineIpAddress, hwnd.Handle);
+        }
+        protected TpipForRasberryPiMachine()
+        {
+            TpipInitialized = false;
+        }
     }
 }
