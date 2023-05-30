031d43904c0fbf5e8255380c0dbc4046b04cc5c8
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-05-30 20:10:42 +0900

    limit algo max angle of grip servo

--- Wimm.Machines.Impl.Algo/Algo.cs
+++ Wimm.Machines.Impl.Algo/Algo.cs
@@ -68,7 +68,7 @@ namespace Wimm.Machines.Impl.Algo
                     ),
                     new AlgoArmServo(
                         "grip_servo", "アームのつかみを扱うサーボ",
-                        ArmDataIndex.Grip, 55, -55, algo
+                        ArmDataIndex.Grip, 0, -55, algo
                     ),
                     new AlgoArmServo(
                         "top_camera_servo", "上部のカメラの回転を扱うサーボ",
