0df6c393aa2ac71ac442a095d78ea45aae182091
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-06-07 21:01:20 +0900

    move Script Folder generation method

--- Wimm/Model/Generator/MachineFolderGenerator.cs
+++ Wimm/Model/Generator/MachineFolderGenerator.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using System.Xml;
+using Wimm.Machines;
 using Wimm.Model.Control;
 
 namespace Wimm.Model.Generator
@@ -50,8 +52,164 @@ namespace Wimm.Model.Generator
                 ));
                 json.WriteTo(metainfo);
             };
-            ScriptDriver.GenerateFolder(machine, scriptDirectory, docsDirectory);
+            GenerateScriptFolder(machine, scriptDirectory, docsDirectory);
             return machineDirectory;
+        }
+
+        private static void GenerateScriptFolder(Machine machine, DirectoryInfo scriptFolder, DirectoryInfo docsFolder)
+        {
+            #region scriptFolder
+            File.Create(scriptFolder.FullName + "/initialize.neo.lua").Close();
+            File.Create(scriptFolder.FullName + "/definition.neo.lua").Close();
+            using (var stream = new StreamWriter(File.Create(scriptFolder.FullName + "/control_map.neo.lua")))
+            {
+                stream.WriteLine("-- コントロールの操作のマッピングを行います。高度な制御がいるならon_controlを使ってネ");
+                stream.WriteLine("-- map(key_array,button_array,action)");
+                stream.WriteLine("-- 引数にbuttonsとkeysが与えられます。");
+                stream.WriteLine("-- buttons : Votice.XInput.GamepadButtons");
+                stream.WriteLine("-- keys : 未定、現在参照できません map関数の入力は受け入れますが何もしません");
+            }
+            using (var stream = new StreamWriter(File.Create(scriptFolder.FullName + "/on_control.neo.lua")))
+            {
+                stream.WriteLine("-- 毎制御ごとに呼び出します。以下引数");
+                stream.WriteLine("-- {Root Module Name} - StructuredModlues これの名前はマシンDLL依存なんだけどよくないかな");
+                stream.WriteLine("-- buttons : Votice.XInput.GamepadButtons - https://github.com/amerkoleci/Vortice.Windows/blob/main/src/Vortice.XInput/GamepadButtons.cs");
+                stream.WriteLine("-- gamepad : Vortice.Xinput.Gamepad - https://github.com/amerkoleci/Vortice.Windows/blob/main/src/Vortice.XInput/Gamepad.cs");
+                stream.WriteLine("-- input : Wimm.Model.Control.Script.InputSupporter");
+                stream.WriteLine("-- wimm : Wimm.Model.Control.Script.WimmFeatureProvider");
+            }
+            var macroFolder = new DirectoryInfo(scriptFolder + "/macro");
+            if (!macroFolder.Exists) { Directory.CreateDirectory(macroFolder.FullName); }
+            using (var macros = new Utf8JsonWriter(File.Create(macroFolder + "/macros.json")))
+            {
+                var data = new JsonObject
+                {
+                    new KeyValuePair<string,JsonNode?>("name",JsonValue.Create("Example Macro")),
+                    new KeyValuePair<string,JsonNode?>("lifetime",JsonValue.Create(5.0))
+                };
+                var example = new JsonObject
+                {
+                    new KeyValuePair<string, JsonNode?>("1",data)
+                };
+                example.WriteTo(macros);
+            };
+            File.Create(macroFolder.FullName + "/1.neo.lua").Close();
+            #endregion
+            #region docs
+            using (var writer = File.Create(docsFolder.FullName + "/Reference.html"))
+            {
+                writer.Write(System.Text.Encoding.UTF8.GetBytes("<!DOCTYPE html>\n"));
+                var document = new XmlDocument();
+                var html = document.CreateElement("html");
+                var head = document.CreateElement("head");
+                {
+                    var charset = document.CreateElement("meta");
+                    charset.SetAttribute("charset", "UTF-8");
+                    charset.IsEmpty = true;
+                    var title = document.CreateElement("title");
+                    title.AppendChild(document.CreateTextNode($"{machine.Name} スクリプト リファレンス"));
+                    head.AppendChild(charset);
+                    head.AppendChild(title);
+                }
+                var body = document.CreateElement("body");
+                {
+                    var structureTitle = document.CreateElement("h2");
+                    structureTitle.AppendChild(document.CreateTextNode("モジュールリスト"));
+                    body.AppendChild(structureTitle);
+                    XmlElement ConstructListItem(ModuleGroup group, string idPrefix)
+                    // idは最終的にName_nameみたいな_区切りになるはず、_を.に置き換えるだけで呼び出せるような形
+                    {
+                        var rootLi = document!.CreateElement("li");
+                        rootLi.AppendChild(document.CreateTextNode(group.Name));
+                        var ul = document.CreateElement("ul");
+                        rootLi.AppendChild(ul);
+                        foreach (var i in group.Children)
+                        {
+                            ul.AppendChild(ConstructListItem(i, idPrefix + i.Name + "_"));
+                        }
+                        foreach (var i in group.Modules)
+                        {
+                            var li = document.CreateElement("li");
+                            var a = document.CreateElement("a");
+                            a.SetAttribute("href", "#" + idPrefix + i.Name);
+                            a.SetAttribute("class", "module");
+                            a.AppendChild(document.CreateTextNode(i.Name));
+                            ul.AppendChild(li);
+                            li.AppendChild(a);
+                        }
+                        return rootLi;
+                    }
+                    var li = ConstructListItem(machine.StructuredModules, "");
+                    var ul = document.CreateElement("ul");
+                    ul.AppendChild(li);
+                    body.AppendChild(ul);
+                    var descriptionTitle = document.CreateElement("h2");
+                    descriptionTitle.AppendChild(document.CreateTextNode("説明"));
+                    void AddDescription(ModuleGroup group, string idPrefix, string namePrefix)
+                    //NamePrefixはモジュールの名前を.で連結したやつ、スクリプトからの参照にそのままつかえるように
+                    {
+                        foreach (var i in group.Children)
+                        {
+                            AddDescription(i, idPrefix + i.Name + "_", namePrefix + i.Name + ".");
+                        }
+                        foreach (var i in group.Modules)
+                        {
+                            var p = document!.CreateElement("p");
+                            var h3 = document.CreateElement("h3");
+                            h3.SetAttribute("class", "module");
+                            h3.SetAttribute("id", idPrefix + i.Name);
+                            h3.AppendChild(document.CreateTextNode(namePrefix + i.Name));
+                            var featureHeader = document.CreateElement("h4");
+                            featureHeader.AppendChild(document.CreateTextNode("機能"));
+                            var featureUl = document.CreateElement("ul");
+                            XmlNode ConstructFeatureListItem(Feature<Delegate> feature)
+                            {
+                                var builder = new StringBuilder();
+                                builder
+                                    .Append(feature.Name)
+                                    .Append('(');
+                                foreach (var f in feature.Function.Method.GetParameters())
+                                {
+                                    builder
+                                        .Append(f.ParameterType)
+                                        .Append(' ')
+                                        .Append(f.Name)
+                                        .Append(',');
+                                }
+                                if (builder[^1] == ',') { builder.Remove(builder.Length - 1, 1); }
+                                builder.Append(')');
+                                var item = document!.CreateElement("li");
+                                item.SetAttribute("class", "feature");
+                                item.AppendChild(document.CreateTextNode(builder.ToString()));
+                                return item;
+                            }
+                            foreach (var f in i.Features)
+                            {
+                                featureUl.AppendChild(ConstructFeatureListItem(f));
+                            }
+                            var descriptionHeader = document.CreateElement("h4");
+                            descriptionHeader.InnerText = "説明";
+                            p.AppendChild(h3);
+                            p.AppendChild(featureHeader);
+                            p.AppendChild(featureUl);
+                            p.AppendChild(descriptionHeader);
+                            p.AppendChild(document.CreateTextNode(i.Description));
+                            body!.AppendChild(document.CreateElement("hr"));
+                            body!.AppendChild(p);
+                        }
+
+                    }
+                    AddDescription(machine.StructuredModules, "", "");
+                }
+                document.AppendChild(html);
+                html.AppendChild(head);
+                html.AppendChild(body);
+                using (var xml = XmlWriter.Create(writer, new XmlWriterSettings() { Encoding = System.Text.Encoding.UTF8 }))
+                {
+                    document.WriteTo(xml);
+                }
+            }
+            #endregion
         }
     }
 }
