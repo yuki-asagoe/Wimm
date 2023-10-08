b39e46ed4e952b2dda83a08d7c05571f15ecbdf5
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-10-08 09:13:58 +0900

    add Audio Input interface

---
+++ Wimm.Machines/Audio/AudioInputEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wimm.Machines.Audio
+{
+    public class AudioInputEventArgs : EventArgs
+    {
+        Channel Channel { get; }
+        /// <summary>
+        /// データへの参照を返します。
+        /// 変数へのキャプチャを推奨します。
+        /// </summary>
+        public Span<byte> Data
+        {
+            get
+            {
+                return Channel.GetData();
+            }
+        }
+        public AudioInputEventArgs(Channel channel)
+        {
+            Channel = channel;
+        }
+    }
+}
---
+++ Wimm.Machines/Audio/Channel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wimm.Machines.Audio
+{
+    public abstract class Channel
+    {
+        public string Name { get; }
+        /// <summary>
+        /// 1サンプル当たりのビット数
+        /// </summary>
+        public int BitsPerSample { get; }
+        /// <summary>
+        /// 1秒あたりのサンプル数
+        /// </summary>
+        public int SamplingRate { get; }
+
+        public event EventHandler<AudioInputEventArgs>? OnDataAvailable;
+
+        public Channel(string name,int bitPerSample,int samplingRate)
+        {
+            Name = name;
+            BitsPerSample = bitPerSample;
+            SamplingRate = samplingRate;
+        }
+        internal abstract Span<byte> GetData();
+    }
+}
