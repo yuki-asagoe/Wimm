c81c908d37c865512292703f92e99879c059974d
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2024-06-26 21:40:43 +0900

    add Converter resouces

---
+++ Wimm/Ui/Converter/InverseBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Wimm.Ui.Converter
+{
+    internal class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b) return !b; else return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
